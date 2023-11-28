903af02db02b6817cd09ef80c4898b40a1e8af67
Gabriele <gabriele.palma.bool@gmail.com>
2023-11-28 11:05:58 +0100

    fix problems chekbutton, create function winner

--- Pari e Dispari/Javascript/Jv.js
+++ Pari e Dispari/Javascript/Jv.js
@@ -17,7 +17,8 @@ const scelta = document.querySelector(".scelta");
 const chekButton = document.getElementById("controllo1");
 
 
-// Quando viene cliccato il pulsante ivia
+
+// Quando viene cliccato il pulsante ivia (1 volta)
 chekButton.addEventListener("click",
 
     function () {
@@ -26,12 +27,187 @@ chekButton.addEventListener("click",
         
         document.getElementById("first").style="display:none"; 
 
-        document.getElementById("second").style="display:block"; 
-
         checkValue(val1);
     }
 );
 
+
+
+
 function checkValue(val1) {
-    if(val1 === "pari")
+
+    //se il valore scelto è pari
+    if(val1 === "Pari"){
+
+        document.getElementById("second").style="display:block"; 
+
+        // Quando viene cliccato il pulsante ivia (2 volta)
+        chekButton.addEventListener("click",
+
+        function () {
+
+            if(document.querySelector(".number").checked === true) {
+
+                const number = document.querySelector(".number");
+
+                let val2 = number.value;
+
+                winner(val1,val2);
+            }
+
+            else if(document.querySelector(".number2").checked === true) {
+
+                const number = document.querySelector(".number2");
+
+                let val2 = number.value;
+
+                winner(val1,val2);
+            }
+
+            else if (document.querySelector(".number3").checked === true) {
+
+                const number = document.querySelector(".number3");
+
+                let val2 = number.value;
+
+                winner(val1,val2);
+            }
+
+        }
+        );
+
+    }
+
+    //se il valore scelto è dispari
+    else{
+
+        document.getElementById("third").style="display:block"; 
+
+        // Quando viene cliccato il pulsante ivia (2 volta)
+        chekButton.addEventListener("click",
+
+        function () {
+
+            if(document.querySelector(".number4").checked === true) {
+
+                const number = document.querySelector(".number4");
+
+                let val2 = number.value;
+
+                winner(val1,val2);
+            }
+
+            else if(document.querySelector(".number5").checked === true) {
+
+                const number = document.querySelector(".number5");
+
+                let val2 = number.value;
+
+                winner(val1,val2);
+            }
+
+            else if (document.querySelector(".number6").checked === true) {
+
+                const number = document.querySelector(".number6");
+
+                let val2 = number.value;
+
+                winner(val1,val2);
+            }
+
+        }
+        );
+
+    }
+    
+}
+
+
+
+
+//funzione per la scelta del vincitore
+function winner(val1, val2){
+
+    //acquisisco scelta del giocatore
+    let TypeU = val1;
+    let valU = val2;
+
+    //acquisico random il numero scelto del pc;
+    let valPc = Math.floor(Math.random() * 7);
+
+    //acquisisco i valore nel tag p(qualora ci fosse)
+    const number = document.querySelector('#box p');
+
+    //Verifico che c'è
+    if(number != null){
+    
+        //rimuovo qualsiasi cosa ci sia
+        Box.removeChild(Box.lastChild);
+    }
+
+    //verifico che il numero scelto dal pc sia pari
+    if(valPc % 2 === 0){
+
+        //se il valore scelto dal pc è maggiore del valore scelto dall'utente
+        if(valPc > valU){
+
+            //Creo l'elemento all'interno del mio file html
+            const stringa = document.createElement ("p");
+
+            //inserisco scritta all'interno del mio contenuto
+            stringa.append("Vince computer con: "+valPc+ " un valore Pari");
+
+            //inserisco stringa all'interno del mio contenitore
+            Box.append(stringa);
+
+        }
+        
+        //in alternativa
+        else {
+
+            //Creo l'elemento all'interno del mio file html
+            const stringa = document.createElement ("p");
+
+            //inserisco scritta all'interno del mio contenuto
+            stringa.append("Vince giocatore con: "+valU+ " un valore "+TypeU);
+
+            //inserisco stringa all'interno del mio contenitore
+            Box.append(stringa);
+
+        }
+
+    }
+
+    //in alternativa
+    else {
+
+        //se il valore scelto dal pc è maggiore del valore scelto dall'utente
+        if (valPc > valU){
+
+            //Creo l'elemento all'interno del mio file html
+            const stringa = document.createElement ("p");
+
+            //inserisco scritta all'interno del mio contenuto
+            stringa.append("Vince computer con: "+valPc+ " un valore Dispari");
+
+            //inserisco stringa all'interno del mio contenitore
+            Box.append(stringa);
+
+        }
+
+        //in alternativa
+        else {
+
+            //Creo l'elemento all'interno del mio file html
+            const stringa = document.createElement ("p");
+
+            //inserisco scritta all'interno del mio contenuto
+            stringa.append("Vince giocatore con: "+valU+ " un valore "+TypeU);
+
+            //inserisco stringa all'interno del mio contenitore
+            Box.append(stringa);
+
+        }
+    }
+
 }
